019de79993d21224b770aa42e12025fe0111362e
David Falusi <falusi.david94@gmail.com>
2022-04-09 18:34:43 +0200

    Clean up fish config

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,9 +1,13 @@
 # language support
 export LANG="en_US"
 export LC_ALL=$LANG.UTF-8
+export LC_CTYPE=$LANG.UTF-8
 
 # configure less
 export LESS="R"
+
+# set editor
+export EDITOR=vim
 
 # set Homebrew paths
 if test (arch) = "i386"
@@ -28,26 +32,17 @@ begin
   end
 end
 
-# clang aliases
-alias cl++11="clear; clang++ -std=c++11 -Wall -Wdeprecated -pedantic"
-alias cl++="clear; clang++ -std=c++99 -Wall -Wdeprecated -pedantic"
-alias cl11="clear; clang -std=c11 -Wall -Wdeprecated -pedantic"
-alias cl="clear; clang -std=c99 -Wall -Wdeprecated -pedantic"
-
 # utils
 alias u1="cd .."
 alias u2="cd ../.."
 alias u3="cd ../../.."
 alias u4="cd ../../../.."
 alias f="fuck"
-alias enablevnc="sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -clientopts -setvnclegacy -vnclegacy yes -clientopts -setvncpw -vncpw mypasswd -restart -agent -privs -all"
-alias disablevnc="sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -configure -access -off"
 alias pwdgenerate="openssl rand -base64 32"
 alias aa="atom ."
 alias ll="ls -lahLG"
 alias lls="ls -lahLSG"
 alias clean_docker='sh -c \'docker rmi $(docker images --quiet --filter "dangling=true")\''
-alias chrcors='open -n -a /Applications/Chromium.app/Contents/MacOS/Chromium  --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security'
 alias de='cd ~/Develop'
 alias ibrew='arch -x86_64 /usr/local/bin/brew'
 alias abrew='arch -arm64 /opt/homebrew/bin/brew'
@@ -55,20 +50,14 @@ alias ifish='arch -x86_64 /usr/local/Cellar/fish/3.3.1/bin/fish'
 
 # ruby/rails utils
 alias be="bundle exec"
-alias rs="bundle exec rspec --color"
-alias rss="bin/rspec --color"
+alias br="bin/rails"
+alias rs="bundle exec rspec"
+alias rss="bin/rspec"
 alias rc="bundle exec rails console"
 alias rcc="bin/rails console"
 alias prep="bundle exec rake db:test:prepare"
 alias rts="bundle exec rake routes"
 alias rtsg="bundle exec rake routes | grep"
-function rt
-  if test -n "$argv"
-    bundle exec rake test TEST=$argv
-  else
-    bundle exec rake test
-  end
-end
 
 # js/npm utils
 alias nrt="npm run test:watch"
@@ -79,6 +68,7 @@ alias nv="npm version"
 alias gpll="git pull --rebase"
 alias gcm="git commit"
 alias gpsh="git push"
+alias gch="git checkout"
 alias gpt="git push origin --tags"
 alias g="git"
 alias gf="git fetch -p"
@@ -102,6 +92,7 @@ end
 alias gcx="git cherry-pick -x"
 alias gca="git commit --amend"
 alias gcp="git cherry-pick"
+alias gup="git stash --include-untracked && git pull --rebase && git stash apply"
 function grdt
   if test -n "$argv"
     env GIT_COMMITTER_DATE=$argv git commit --amend --date=$argv --no-edit;
